8e65b3c57a038834b7667b0f00f982d3aa54f647
sam88an@gmail.com <sam88an@gmail.com>
2021-03-18 00:20:42 +0700

    first commit

---
+++ controllers/userCtrl.js
@@ -0,0 +1,69 @@
+const Users = require("../models/userModel");
+const bycryt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const userCtrl = {
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (user)
+        return res.status(400).json({ msg: "The email already exists." });
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Password is at least 6 characters long." });
+      // Password Encryption
+      const passwordHash = await bycryt.hash(password, 10);
+      // Save mongodb
+
+      const newUser = new Users({ name, email, password: passwordHash });
+      await newUser.save();
+      const accesstoken = createAccessToken({ id: newUser._id });
+      const refreshtoken = createRefreshToken({ id: newUser._id });
+      res.cookie("refreshtoken", refreshtoken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+      });
+      res.json({ accesstoken: accesstoken });
+      // Then create jsonwebtoken to authencation
+      //success
+      //   res.json({ msg: "Register Success !" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  refreshToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token)
+        return res.status(4).json({ msg: "Please Login or Register." });
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(400).json({ msg: err.message });
+        const accesstoken = createAccessToken({ id: user.id });
+        res.json({ user, accesstoken });
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (!user) return res.status(400).json({ msg: "User does not exist" });
+      const isMatch = await bycryt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Incorrect password" });
+      // If login success , create access token and refresh token
+      res.json({ msg: "Login success..." });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+};
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+};
+module.exports = userCtrl;
