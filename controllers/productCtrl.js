482deb9ffe839c2f8dcc106f4ec7b4d77ac4efca
sam88an@gmail.com <sam88an@gmail.com>
2021-03-21 18:42:20 +0700

    Make some page

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,10 +1,58 @@
 const Products = require("../models/productModel");
+// Filter ,sorting and paginating
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filtering() {
+    const queryObj = { ...this.queryString };
+    console.log({ before: queryObj }); //before delete page
+    const excludedFields = ["page", "sort", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    console.log({ after: queryObj }); //after delete page
+    let queryStr = JSON.stringify(queryObj);
+    console.log({ queryObj, queryStr });
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
+    this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
 
+    return this;
+  }
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 9;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
 const productCtrl = {
   getProducts: async (req, res) => {
     try {
-      const products = await Products.find();
-      res.json(products);
+      console.log(req.query);
+      const features = new APIfeatures(Products.find(), req.query)
+        .filtering()
+        .sorting()
+        .paginating();
+      const products = await features.query;
+
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
